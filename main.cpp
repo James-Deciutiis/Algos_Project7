a41468fbf754fe0ef294ee97dfcd4427e1cd6bd5
james-deciutiis <jamesdeciutiis@gmail.com>
2021-04-23 12:39:18 -0700

    final commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <fstream>
 #include <vector>
 
+using namespace std;
 
 class UEdge{
 	public:
@@ -9,12 +11,16 @@ class UEdge{
 		int cost;
 		UEdge *next;
 
-		UEdge(int Ni_, int Nj_, int cost_) : Ni(Ni_) , Nj(Nj_), cost(cost_){
-			UEdge head = new UEdge(Ni, Nj, cost, NULL);
+		UEdge(int Ni_, int Nj_, int cost_, UEdge *next_) : Ni(Ni_), Nj(Nj_), cost(cost_), next(next_){
 		}
 
-		void printEdge(UEdge edge, ofstream debugFile){
-			
+		void printEdge(ofstream& debugFile){
+			if(next){
+				debugFile<<"<Ni: "<< Ni << " Nj: " << Nj << " Cost: " << cost << " Next Node: " << next->Ni << ">-->"; 			
+			}
+			else{
+				debugFile<<"<Ni: "<< Ni << " Nj: " << Nj << " Cost: " << cost << " Next Node: NULL>";
+			}			
 		}
 };
 
@@ -22,82 +28,173 @@ class PrimMST{
 	public:
 		int numNodes;
 		int nodeInSetA;
-		int whichSet[];
+		int*  whichSet;
 		UEdge* edgeListHead;
 		UEdge* MSTlistHead;
 		int totalMSTCost = 0;
 		
-		PrimMSt(int numNodes_, int nodeInSetA_) : numNodes(numNodes_), nodeInSetA(nodeInSetA_){
+		PrimMST(int numNodes_, int nodeInSetA_) : numNodes(numNodes_), nodeInSetA(nodeInSetA_){
 			whichSet = new int[numNodes + 1];
 		 	whichSet[nodeInSetA] = 1;	
+			edgeListHead = new UEdge(0, 0, 0, NULL);
+			MSTlistHead = new UEdge(0, 0, 0, NULL);
 		}
 
 		void listInsert(UEdge* newEdge){
+			UEdge* scanner = edgeListHead;
+			while(scanner->next && scanner->next->cost < newEdge->cost){
+				scanner = scanner->next;
+			}
+
+			newEdge->next = scanner->next;
+			scanner->next = newEdge;
 		}
 
 		UEdge* removeEdge(){
+			UEdge* scanner = edgeListHead;
+			UEdge* retval = scanner;
+			while(scanner->next){
+				if((whichSet[scanner->next->Ni] != whichSet[scanner->next->Nj]) && (whichSet[scanner->next->Ni] == 1 || whichSet[scanner->next->Nj] == 1)){
+					retval = scanner->next;
+					scanner->next = scanner->next->next;
+					return retval;
+				}
+				scanner = scanner->next;
+			}
+
+			return retval;
 		}
 
 		void addEdge(UEdge* newEdge){
+			newEdge->next = MSTlistHead->next;
+			MSTlistHead->next = newEdge;
 		}
 
-		void printSet(){
+		void printSet(ofstream& debugFile){
+			debugFile<<"*** PRINTING WHICHSET ***"<<endl;
+			for(int i = 0; i < numNodes + 1; i++){
+				debugFile<<" "<<whichSet[i];
+			}
+
+			debugFile<<endl<<"*** DONE PRINTING WHICHSET ***"<<endl;
 		}
 
-		void printEdgeList(){
+		void printEdgeList(ofstream& debugFile){
+			UEdge* scanner = edgeListHead;
+			debugFile<<"*** PRINTING EDGE LIST ***"<<endl;
+			while(scanner){
+				scanner->printEdge(debugFile);
+				scanner = scanner->next;
+			}	
+			debugFile<<endl<<"*** DONE PRINTING EDGE LIST ***"<<endl;
 		}
 
-		void printMSTList(ofstream& outFile){
+		void printMSTList(ofstream& debugFile){
+			UEdge* scanner = MSTlistHead;
+			debugFile<<"*** PRINTING MST LIST ***"<<endl;
+			while(scanner){
+				scanner->printEdge(debugFile);
+				scanner = scanner->next;
+			}	
+			debugFile<<endl<<"*** DONE PRINTING MST LIST ***"<<endl;
+		}
+
+		void updateMST(UEdge* newEdge){
+			addEdge(newEdge);
+			totalMSTCost += newEdge->cost;
+			if(whichSet[newEdge->Ni] == 1){
+				whichSet[newEdge->Nj] = 1;
+			}
+			else{
+				whichSet[newEdge->Ni] = 1;
+			}
 		}
 
 		bool setBisEmpty(){
+			for(int i = 0; i < numNodes+1; i++){
+				if(whichSet[i] != 1){
+					return false;
+				}
+			}
 
 			return true;
 		}
 };
 
-int main(int argc, char*[]argv){
+int main(int argc, char* argv[]){
 	ifstream inFile(argv[1]);
-	int nodeInSetA(argv[2]);
+	int nodeInSetA(stoi(argv[2]));
+	ofstream MSTFile(argv[3]);
+	ofstream debugFile(argv[4]);
+
 	string line;
-	string num;
 	int numNodes;
-	PrimMST prim;
-	index = 0;
-	while(nextLine(inFile, line)){
-		num = "";
-		if(index = 0){
+	int index = 0;
+	PrimMST* prim;
+	while(getline(inFile, line)){
+		if(index == 0){
+			string num = "";
 			for(auto c : line){
 				if(c!=' '){
 					num+=c;
 				}
-				else{
-					numNodes = stoi(num);
-				}
 			}
-			
-		prim = new PrimMST(numNodes, nodeInSetA);
-		prim.printSet();
-	}
-}	
-			
-			
-					
 				
+			numNodes = stoi(num);
+			index++;	
+			prim = new PrimMST(numNodes, nodeInSetA);
+			prim->printSet(debugFile);
+			continue;
+		}
+		
 
+		vector<int> param = {};
+		string curr = "";
+		for(auto c : line){
+			if(c!=' '){
+				curr += c;
+			}
+			else{
+				param.push_back(stoi(curr));
+				curr = "";
+			}
+		}
+				
+		param.push_back(stoi(curr));
 
-
-	return 0;
-}
-
+		int Ni = param[0];
+		int Nj = param[1];
+		int edgeCost = param[2];
+		UEdge* newEdge = new UEdge(Ni, Nj, edgeCost, NULL);
+		prim->listInsert(newEdge);
+		prim->printEdgeList(debugFile);
+	}
 	
-	
+	while(!prim->setBisEmpty()){
+		UEdge* nextEdge = prim->removeEdge();
 
+		debugFile<<endl;
+		nextEdge->printEdge(debugFile);
+		debugFile<<endl;
 
+		if(nextEdge->Ni == 0 && nextEdge->Nj ==0){
+			break;
+		}
 
+		prim->updateMST(nextEdge);
+		prim->printSet(debugFile);
+		prim->printEdgeList(debugFile);
+		prim->printMSTList(debugFile);
+	}	
 
+	MSTFile << "*** Prim's MST of he input graph G is: ***" << endl;
+    MSTFile << "numNodes: " << numNodes << endl;
+	prim->printMSTList(MSTFile);
+	MSTFile << "*** MST total cost = " << prim->totalMSTCost<<" ***";	
 
-
+	inFile.close();
+	MSTFile.close();
+	debugFile.close();
 
 	return 0;
 }
